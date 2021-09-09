7eb941607162599c80ae796aa110d7a60f99aaac
Cava <89451089+AchievementCava@users.noreply.github.com>
2021-09-09 23:58:52 +0800

    minor tweaks made to js, switched font-size units from vw/vh to rem and added pulsing background colour

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,9 +2,26 @@ var navbar = document.getElementById("menu");
 
 function closeNavbar(){
     navbar.style.top = "-300vh";
+    unlockScroll();
 }
 
 function openNavbar(){
     navbar.style.top = "0vh";
+    window.scrollTo({top: 0});
+    sleep(10000);
+    lockScroll();
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function lockScroll(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);
+    };
+}
+
+function unlockScroll(){
+    window.onscroll=function(){};
+}
